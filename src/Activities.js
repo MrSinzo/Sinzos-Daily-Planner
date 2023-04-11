7b72383a6e814a86ed64a309e2cbfedfd93a161d
Kyle Feldman <flip93wolf@gmail.com>
2023-04-11 11:53:10 -0700

    got picture placement set up for buttons, altered button styles for better UI

--- src/Activities.js
+++ src/Activities.js
@@ -1,86 +1,104 @@
+import placeholder from './images/placeholder.png'
+
 const activities = [
   {
     id: 1,
     name: "Bus to Class",
     time: "9:00-9:15",
+    pic: placeholder
   },
   {
     id: 2,
     name: "Puzzles",
     time: "9:15-9:30",
+    pic: placeholder
   },
   {
     id: 3,
     name: "Writing",
     time: "9:30-9:45",
+    pic: placeholder
   },
   {
     id: 4,
     name: "Task Boxes",
     time: "9:45-10:00",
+    pic: placeholder
   },
   {
     id: 5,
     name: "Specialist's",
     time: "10:05-10:35",
     break: "V Break 10:05-10:20",
+    pic: placeholder
   },
   {
     id: 6,
     name: "Task Boxes or Art",
     time: "10:40-11:00",
-    break: "C Break 10:45-11:00"
+    break: "C Break 10:45-11:00",
+    pic: placeholder
   },
   {
     id: 7,
     name: "Lunch",
     time: "11:00-11:30",
+    pic: placeholder
   },
   {
     id: 8,
     name: "Recess",
     time: "11:30-11:55",
+    pic: placeholder
   },
   {
     id: 9,
     name: "Math At Tables",
     time: "12:00-12:15",
-    break: "A Break 12:30-1:15"
+    break: "A Break 12:30-1:15",
+    pic: placeholder
   },
   {
     id: 10,
     name: "Circle / Math",
     time: "12:15-12:45",
+    pic: placeholder
   },
   {
     id: 11,
     name: "Center time",
     time: "12:45-1:15",
+    pic: placeholder
   },
   {
     id: 12,
     name: "Reading Circle",
     time: "1:15-1:45",
+    pic: placeholder
   },
   {
     id: 13,
     name: "Rest Time",
     time: "1:45-2:00",
+    pic: placeholder
   },
   {
     id: 14,
     name: "Reading",
     time: "2:00-2:30",
+    pic: placeholder
   },
   {
     id: 15,
     name: "Circle songs",
     time: "2:30-3:00",
+    pic: placeholder
   },
   {
     id: 16,
     name: "Snack and Go Home!",
     time: "3:00-3:30",
+    pic: placeholder
   },
 ];
 
--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Draggable from "react-draggable";
 
+
+
 export default function ActivityList({ activities }) {
   return (
     <div className="flexThis">
-    {activities.map((activity) => (
-      <Draggable key={activity.id} axis="both" defaultPosition={{ x: 0, y: 0 }}>
-        <div className="draggableActivity">
-          <li className="activityText">{activity.name}</li>
-          <li>{activity.time}</li>
-          <li>{activity.break}</li>
-        </div>
-      </Draggable>
-    ))}
-  </div>
-  )
-
+      {activities.map((activity) => (
+        <Draggable
+          key={activity.id}
+          axis="both"
+          defaultPosition={{ x: 0, y: 0 }}
+        >
+          <div className="draggableActivity">
+            {/* <img className="picFix" src={activity.pic} /> */}
+            <li className="activityText">{activity.name}</li>
+            <li>{activity.time}</li>
+            <li>{activity.break}</li>
+            <img className="picFix" src={activity.pic} />
+          </div>
+        </Draggable>
+      ))}
+    </div>
+  );
 }
